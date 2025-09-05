a4776df71102a99ee4ba09658793b7df3ce2eeea
Angel Torres <luis.vidalto@alumno.buap.mx>
2025-09-05 00:23:57 -0600

    feat: add route for NewAccount component in main application

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,13 @@ import { BrowserRouter, Route, Routes } from "react-router";
 
 import "./index.css";
 import Login from "./components/pages/Login.tsx";
+import NewAccount from "./components/pages/NewAccount.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route index path="/login" element={<Login />} />
-      {/* <Route path="/list" element={<TodoApp />} /> */}
+      <Route path="/new-account" element={<NewAccount />} />
     </Routes>
   </BrowserRouter>
 );
