5aed3a1c5064c4835afe4696912e70be5b06eea9
Angel Torres <luis.vidalto@alumno.buap.mx>
2025-09-04 22:20:07 -0600

    feat: add LoginHero component for user identification

---
+++ frontend/src/components/login/LoginHero.tsx
@@ -0,0 +1,8 @@
+export const LoginHero = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 mt-20">
+      <img src="../../../assets/login 1.svg" alt="Gato animado que duda" />
+      <p className="text-4xl font-semibold">Who are you?</p>
+    </div>
+  );
+};
