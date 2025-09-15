ead04dd103bfbebab1733d92adbb00145b661ca3
Angel Torres <luis.vidalto@alumno.buap.mx>
2025-09-15 16:49:20 -0600

    feat: refactor Logo component to LogoTitle with className prop for styling

--- frontend/src/components/login/LogoTitle.tsx
+++ frontend/src/components/login/LogoTitle.tsx
@@ -1,10 +1,15 @@
-export const Logo = () => {
+interface Props {
+  className?: string;
+}
+
+export const LogoTitle = ({ className }: Props) => {
   return (
-    <div className="flex items-center justify-center gap-4 mt-8">
+    <div className={`flex items-center justify-center gap-4 mt-4 ${className}`}>
       <img
         src="/assets/logo-v2.svg"
         alt="Logo de la página Clawver Tasks"
         fetchPriority="high"
+        className="w-16"
       />
       <h1 className="font-bold text-[#222] text-4xl ">Clawver Tasks</h1>
     </div>
