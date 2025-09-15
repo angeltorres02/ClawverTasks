0d1643b5c5c61464a8f2d453dbe45aac9c9fd1ea
Angel Torres <luis.vidalto@alumno.buap.mx>
2025-09-15 16:49:09 -0600

    feat: restructure routing to include MainLayout and MainPage components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,16 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 import Login from "./components/pages/Login.tsx";
 import NewAccount from "./components/pages/NewAccount.tsx";
+import MainLayout from "./layouts/MainLayout.tsx";
+import MainPage from "./components/pages/MainPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
-      <Route index path="/login" element={<Login />} />
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<MainPage />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
       <Route path="/new-account" element={<NewAccount />} />
     </Routes>
   </BrowserRouter>
